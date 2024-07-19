b227fecbdd0c987d19f32e7c0e112f2fb4963469
Kanishk <kanishka.ranjan.9@gmail.com>
2024-07-19 15:19:18 +0530

    signup signin done

---
+++ frontend/src/components/Auth.tsx
@@ -0,0 +1,17 @@
+export const Auth= ()=>{
+    return <div className="h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div>
+                <div className="text-3xl font-extrabold">
+                    Create an account
+                </div>
+                <div className="text-slate-400">
+                    Already have an account?
+                    <Link>Login</Link> 
+                </div>
+                
+            </div>
+            
+        </div>
+    </div>
+}
